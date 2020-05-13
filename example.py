70e5dde39837f12eb7fdd532dfd6fcaa67ba9f47
EnginKosure <engin.kosure@cronos.be>
2020-05-13 13:17:39 +0200

    find pi and e functions has been added to example file

--- example.py
+++ example.py
@@ -7,15 +7,14 @@ print(math.e)
 # Keep a limit to how far the program will go.
 def special_pi(input):
     if input==0:
-        print(str(math.pi)[:input+1])
+        print('pi', str(math.pi)[:input+1])
     else:
-        print(str(math.pi)[:input+2])
+        print('pi', str(math.pi)[:input+2])
 
 
 
 
 # user_input =0
-
 
 def take_input():
 
@@ -32,4 +31,29 @@ def take_input():
     return user_input
 
 special_pi(take_input())
+
+# Find e to the Nth Digit - Just like the previous problem, but with e instead of PI.
+# Enter a number and have the program generate e up to that many decimal places.
+# Keep a limit to how far the program will go.
+
+def special_e(input):
+    if input==0:
+        print('e', str(math.e)[:input+1])
+    else:
+        print('e', str(math.e)[:input+2])
+
+def take_input_for_e():
+    while True:
+        try:
+            input_e=int(input('Enter a number between 0-15: '))
+        except ValueError:
+            print('It must be an integer!')
+        else:
+            if input_e<0 or input_e>15:
+                print('It must be between 0-15')
+            else:
+                break
+    return input_e
+
+special_e(take_input_for_e())
 
